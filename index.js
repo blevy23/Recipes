78498ae260f41872200d7504264a34de96c20fea
Moho2023 <90466441+Moho2023@users.noreply.github.com>
2022-12-15 11:48:40 -0500

    stuff

---
+++ index.js
@@ -0,0 +1,28 @@
+//..............Include Express..................................//
+const express = require('express');
+const fs = require('fs');
+const ejs = require('ejs');
+
+//..............Create an Express server object..................//
+const app = express();
+
+//..............Apply Express middleware to the server object....//
+app.use(express.json()); //Used to parse JSON bodies (needed for POST requests)
+app.use(express.urlencoded());
+app.use(express.static('public')); //specify location of static assests
+app.set('views', __dirname + '/views'); //specify location of templates
+app.set('view engine', 'ejs'); //specify templating library
+
+//.............Define server routes..............................//
+//Express checks routes in the order in which they are defined
+
+app.get('/', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("index");
+});
+
+const port = 3000;
+app.listen(port, function() {
+  console.log('Server started at http://localhost:'+port+'.')
+});
